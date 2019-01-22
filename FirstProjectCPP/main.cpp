bc40965c971abc4d14974a28ff13a28253c93add
Caroline Jarman <carolinerosejarman@gmail.com>
2019-01-22 08:47:03 -0700

    Initial Commit

---
+++ FirstProjectCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FirstProjectCPP
+//
+//  Created by Jarman, Caroline on 1/22/19.
+//  Copyright © 2019 ctec. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
