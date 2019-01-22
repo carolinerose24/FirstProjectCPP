d01d6a060f792fa7ca892582f57153962a702392
Caroline Jarman <carolinerosejarman@gmail.com>
2019-01-22 09:16:43 -0700

    Added comments

--- FirstProjectCPP/main.cpp
+++ FirstProjectCPP/main.cpp
@@ -6,10 +6,28 @@
 //  Copyright © 2019 ctec. All rights reserved.
 //
 
-#include <iostream>
+#include <iostream> //like an import stement for java and swift
+    // <> for library level imports, for compile code for nuilt in brackets
+// # include how you link files in you project must not be compiled
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+
+using namespace std; //can  now use cout throughout the program
+int main(int argc, const char * argv[])
+{
+    // insert code here... Main method is an int method
+
+    //std::
+    cout << "C++ & Data Structures/Algorithms \n"; //:: = scope resolution operater --> the C out object lives inside the scope res. library
+    //OR a data member of, --> show belonging
+    //    << = the output stream operator
+    //stuff on right side gest dumped into the left side ^^C OUT
+    //points to where it is going
+    //>> for input, << for output
+    return 0; //return 0 if it is executed properly-> will sometimes work even w/o this
+    //Strings -> char arrays?, NO booleans --> everything that is not 0 is true
+    
+    //priority queue is like a fast pass
+    //declare app, run start
+    
+    
 }
